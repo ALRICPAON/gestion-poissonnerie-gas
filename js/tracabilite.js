76f0893b6b834fac3d0678f61f6db753e233e752
ALRICPAON <paonalric@gmail.com>
2025-11-21 19:56:12 +0100

    Update tracabilite.js

--- js/tracabilite.js
+++ js/tracabilite.js
@@ -292,6 +292,10 @@ function renderCards(cards, typeFilter) {
           <strong>Poids initial :</strong> ${poidsInitial} kg<br>
           <strong>Zone :</strong> ${lot.zone || ligne?.zone || ""} ${lot.sousZone || ligne?.sousZone || ""}<br>
           <strong>Engin :</strong> ${lot.engin || ligne?.engin || ""}<br>
+          ${ lot.photo_url ? `
+  <br><img class="trace-photo" src="${lot.photo_url}">
+` : "" }
+
           ${ lot.source === "transformation" && lot.origineLots ? `
   <strong>Origine :</strong><br>
   ${lot.origineLots.map(o => `• Lot ${o.lotId} : ${o.kgPris}kg`).join("<br>")}
